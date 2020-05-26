5f2054e29bfcbe40c898248da097cada4969926d
Deni <denisagal09@yahoo.com>
2020-05-26 13:26:27 +0300

    Pls work

--- src/main/java/TA/controllers/LearnerMenuController.java
+++ src/main/java/TA/controllers/LearnerMenuController.java
@@ -1,9 +1,5 @@
 package TA.controllers;
 
-import TA.model.User;
-import TA.services.UserService;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
@@ -17,23 +13,19 @@ public class LearnerMenuController {
         subject.getItems().addAll("Art", "Biology", "Chemestry", "English", "History", "Math", "Physics", "Programming");
     }
 
-    UserService users = (UserService) getUsersList();
-    public String username;
-
-    public void setUsername(String username){
-        this.username = username;
-    }
+    //UserService users = (UserService) getUsersList();
+    
 
     public void setSubjectPressed(ActionEvent actionEvent){
         System.out.println((String) subject.getValue());
-        users.addSubject(username, (String) subject.getValue());
+        //users.addSubject(user_name, (String) subject.getValue());
     }
 
-    public ObservableList<User> getUsersList() {
+    /*public ObservableList<User> getUsersList() {
         ObservableList<User> usersList = FXCollections.observableArrayList();
 
         usersList.addAll(UserService.getUsers());
 
         return usersList;
-    }
+    }*/
 }
